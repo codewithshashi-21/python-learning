2bda42e0669476c0909bd1ee0a7d58028f11f2de
codewithshashi-21 <shashivardhan93906@gmail.com>
2025-11-05 18:47:51 +0530

    Day 2: variables, input, casting, operators

---
+++ day2.py
@@ -0,0 +1,25 @@
+#variables and types
+name = "Sunny"
+age = "21"
+height = "5'4"
+print(name,age,height)
+#input
+user_name = input("Enter your name: ")
+print("Welcome",user_name)
+#arthematic operatios
+a = "10"
+b = "20"
+a = int(a)
+b = int(b)
+print(a+b)
+
+x = 10
+y = 3
+
+print("Add:", x + y)
+print("Sub:", x - y)
+print("Mul:", x * y)
+print("Div:", x / y)
+print("Floor Div:", x // y)
+print("Mod:", x % y)
+print("Power:", x ** y)
